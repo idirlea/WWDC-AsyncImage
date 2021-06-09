2904543a3ec827af5946695c50594d83b446c454
Ionut Dirlea <ionut.dirlea@gmail.com>
2021-06-09 09:33:47 +0300

    Async Image

--- WWDC-AsyncImage/ContentView.swift
+++ WWDC-AsyncImage/ContentView.swift
@@ -7,13 +7,23 @@
 
 import SwiftUI
 
+@available(iOS 15.0, *)
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        AsyncImage(url: URL(string: "https://cdn.pixabay.com/photo/2016/11/22/23/14/terrier-1851108_1280.jpg")) { image in
+            image
+                .resizable()
+                .scaledToFit()
+                .mask(RoundedRectangle(cornerRadius: 20))
+                .padding()
+        } placeholder: {
+            Text("Loading image...")
+        }
+            
     }
 }
 
+@available(iOS 15.0, *)
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
