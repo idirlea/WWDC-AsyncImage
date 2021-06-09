6bf32a5a2cc005b7bdad7787ea782b7acbcc05e4
Ionut Dirlea <ionut.dirlea@gmail.com>
2021-06-09 08:20:34 +0300

    Initial Commit

---
+++ WWDC-AsyncImage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WWDC-AsyncImage
+//
+//  Created by Ionut Dirlea on 09.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WWDC-AsyncImage/WWDC_AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC_AsyncImageApp.swift
+//  WWDC-AsyncImage
+//
+//  Created by Ionut Dirlea on 09.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct WWDC_AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
